dd481c6140c811139baad6f8b548d196472d1ac9
Ruben <rwoldhui@avans.nl>
2019-01-26 18:57:24 +0100

    [Added] game resetting

--- src/Game/Models/Bird.java
+++ src/Game/Models/Bird.java
@@ -2,36 +2,29 @@ package Game.Models;
 
 import Game.UI.GameConstants;
 import NN.NeuralNetwork;
-import com.sun.istack.internal.Nullable;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
 public class Bird implements IDrawable {
     private NeuralNetwork brain;
     private Vector2D position;
     private Vector2D velocity;
     private Vector2D acceleration;
-    private int size, score, fitness;
+    private int size, neuron_inputs;
+    private float score, fitness;
 
-    public Bird(@Nullable NeuralNetwork brain) {
-
+    public Bird(NeuralNetwork brain) {
         this.position = new Vector2D(GameConstants.WIDTH / 4f, GameConstants.HEIGHT / 2f);
-        //   this.position = position;
+        this.neuron_inputs = 4;
         this.velocity = new Vector2D(0, 0);
         this.acceleration = new Vector2D(0, 0);
         this.size = 50;
         if (brain != null) {
-            this.brain.mutate((float e, int i, int j) -> {
-                if (Math.random() < 0.1) {
-                    float offset = (float) (Math.random() * 0.5);
-                    return e + offset;
-                } else {
-                    return e;
-                }
-            });
+            this.brain = brain;
         } else {
-            this.brain = new NeuralNetwork(5, 8, 2);
+            this.brain = new NeuralNetwork(neuron_inputs, 8, 2);
         }
         this.score = 0;
         this.fitness = 0;
@@ -50,6 +43,7 @@ public class Bird implements IDrawable {
         position.add(velocity);
         velocity.add(acceleration);
         acceleration.mult(0);
+        score++;
     }
 
     public void up() {
@@ -64,6 +58,8 @@ public class Bird implements IDrawable {
                 System.out.println("Hitting top with pipe Y-position: " + (pipe.getPosition().getY() - pipe.getSize() + size));
                 collision = true;
             }
+        } else if (this.getPosition().getY() < 0 || this.getPosition().getY() > GameConstants.HEIGHT - 50) {
+            collision = true;
         } else {
             if (pipe.getPosition().getX() == this.position.getX() && (pipe.getPosition().getY() - size / 2) < this.position.getY()) {
                 System.out.println("Hitting bottom with bird Y-position: " + this.position.getY());
@@ -71,6 +67,7 @@ public class Bird implements IDrawable {
                 collision = true;
             }
         }
+
         return collision;
     }
 
@@ -83,5 +80,60 @@ public class Bird implements IDrawable {
     @Override
     public Vector2D getPosition() {
         return position;
+    }
+
+    public void think(ArrayList<Pipe> pipes) {
+        Pipe closest = null;
+        float record = 10000000000000f;
+        for (int i = 0; i < pipes.size(); i++) {
+            float diff = pipes.get(i).getPosition().getX() - this.position.getX();
+            if (diff > 0 && diff < record) {
+                record = diff;
+                closest = pipes.get(i);
+            }
+        }
+
+        if (closest != null) {
+            float[] inputs = new float[neuron_inputs];
+            if (closest.isTop()) {
+                inputs[0] = closest.getPosition().getX() / GameConstants.WIDTH;
+                inputs[1] = closest.getPosition().getY() / GameConstants.HEIGHT;
+                inputs[2] = (closest.getPosition().getY() - 200) / GameConstants.HEIGHT;
+                inputs[3] = this.position.getY() / GameConstants.HEIGHT;
+            } else {
+                inputs[0] = closest.getPosition().getX() / GameConstants.WIDTH;
+                inputs[1] = (closest.getPosition().getY() + 200) / GameConstants.HEIGHT;
+                inputs[2] = closest.getPosition().getY() / GameConstants.HEIGHT;
+                inputs[3] = this.position.getY() / GameConstants.HEIGHT;
+            }
+
+            float[] action = this.brain.predict(inputs);
+            if (action[1] > action[0])
+                this.up();
+            //   System.out.println("Action: " + action[0] + " | " + action[1]);
+
+        }
+
+
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public float getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(float fitness) {
+        this.fitness = fitness;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    public NeuralNetwork getBrain() {
+        return brain;
     }
 }
--- src/Game/UI/GameWindow.java
+++ src/Game/UI/GameWindow.java
@@ -15,67 +15,46 @@ import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
 
 public class GameWindow extends JPanel implements KeyListener {
-    private int width, height;
+    private int width, height, in_training;
     private Timer drawLoop;
     private Timer gameLoop;
 
     private Vector2D gravity;
 
-    private Bird bird;
+    private ArrayList<Bird> birds;
     private ArrayList<Pipe> pipes;
 
 
     private long deltaTime, startTime, endTime;
 
     private int score, counter, gap;
-
+    private ArrayList<Bird> savedBirds;
 
     public GameWindow() {
         width = 1280;
         height = 720;
-        bird = new Bird(null);
-
-        generateFrame();
-        pipes = new ArrayList<>();
         gap = 200;
+        in_training = 500;
+        pipes = new ArrayList<>();
+        birds = new ArrayList<>();
+        savedBirds = new ArrayList<>();
+        for (int i = 0; i < in_training; i++)
+            birds.add(new Bird(null));
+        //   addPipes();
+        generateFrame();
         gravity = new Vector2D(0f, 0.2f);
+        reset();
+    }
 
-        drawLoop = new Timer(1000 / GameConstants.TARGET_FPS, event -> {
-            repaint();
-        });
-        gameLoop = new Timer(1000 / (GameConstants.TARGET_FPS), event -> {
-            score++;
-            counter++;
-            CompletableFuture.runAsync(() -> {
-                bird.applyForce(gravity);
-                bird.update();
-                getPipes().forEach(IDrawable::update);
-            });
+    private synchronized ArrayList<Bird> getBirds() {
+        return this.birds;
+    }
 
-
-            if (counter > 60) {
-                int topSize = (int) (Math.random() * (height / 2));
-                int bottomSize = height - topSize - gap;
-                pipes.add(new Pipe(true, width, height, topSize));
-                pipes.add(new Pipe(false, width, height, bottomSize));
-                counter = 0;
-            }
-            CompletableFuture.runAsync(() -> {
-                if (bird.getPosition().getY() < 0 || bird.getPosition().getY() > height - 50) {
-                    onGameEnded();
-                }
-                for (Pipe pipe : pipes) {
-                    if (bird.hasCollision(pipe)) {
-                        onGameEnded();
-                        break;
-                    }
-                }
-            });
-
-            getPipes().removeIf(pipe -> pipe.getPosition().getX() < -(width / 10));
-        });
-        drawLoop.start();
-        gameLoop.start();
+    private void addPipes() {
+        int topSize = (int) (Math.random() * (height / 2));
+        int bottomSize = height - topSize - gap;
+        pipes.add(new Pipe(true, width, height, topSize));
+        pipes.add(new Pipe(false, width, height, bottomSize));
     }
 
     private synchronized ArrayList<Pipe> getPipes() {
@@ -95,10 +74,65 @@ public class GameWindow extends JPanel implements KeyListener {
         frame.setVisible(true);
     }
 
-    private void onGameEnded() {
-        drawLoop.stop();
-        gameLoop.stop();
-        System.out.println("You're dead. Score: " + score);
+    private void onGameEnded(Bird bird) {
+        getBirds().remove(bird);
+        savedBirds.add(bird);
+        if (getBirds().size() == 0) {
+            gameLoop.stop();
+            drawLoop.stop();
+            getPipes().clear();
+            addPipes();
+            nextGeneration();
+            reset();
+        }
+
+        //    System.out.println("You're dead. Score: " + score);
+    }
+
+    private void reset() {
+        System.out.println(getBirds().size());
+        score = 0;
+        counter = 0;
+        drawLoop = new Timer(1000 / GameConstants.TARGET_FPS, event -> {
+            repaint();
+        });
+        gameLoop = new Timer(1000 / (GameConstants.TARGET_FPS), event -> {
+            score++;
+            counter++;
+            CompletableFuture.runAsync(() -> {
+                for (Bird bird : getBirds()) {
+                    bird.applyForce(gravity);
+                    bird.update();
+                }
+                getPipes().forEach(IDrawable::update);
+
+            });
+            CompletableFuture.runAsync(() -> {
+                for (Bird bird : getBirds()) {
+                    bird.think(pipes);
+                }
+            });
+
+            if (counter > 60) {
+                addPipes();
+                counter = 0;
+            }
+            CompletableFuture.runAsync(() -> {
+                for (Bird bird : getBirds()) {
+
+                    for (Pipe pipe : pipes) {
+                        if (bird.hasCollision(pipe)) {
+                            onGameEnded(bird);
+                            break;
+                        }
+                    }
+                }
+            });
+
+            getPipes().removeIf(pipe -> pipe.getPosition().getX() < -(width / 10));
+        });
+        drawLoop.start();
+        gameLoop.start();
     }
 
     @Override
@@ -108,7 +142,7 @@ public class GameWindow extends JPanel implements KeyListener {
         g2d.setColor(Color.BLACK);
         g2d.fill(new Rectangle2D.Double(0, 0, getWidth(), getHeight()));
 
-        bird.draw(g2d);
+        getBirds().forEach(bird -> bird.draw(g2d));
         getPipes().forEach(p -> p.draw(g2d));
 
         drawFPS(g2d);
@@ -122,7 +156,7 @@ public class GameWindow extends JPanel implements KeyListener {
 
         String fps = "FPS: ";
         if (deltaTime > 0)
-            fps += Double.toString(1000 / deltaTime);
+            fps += Double.toString(1000f / deltaTime);
         //lastFPS = fps;
         g2d.setColor(Color.WHITE);
         g2d.setFont(new Font(g2d.getFont().getFontName(), Font.PLAIN, 20));
@@ -143,11 +177,46 @@ public class GameWindow extends JPanel implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            bird.up();
+            //  bird.up();
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
     }
+
+
+    private void nextGeneration() {
+        normalizeFitness(savedBirds);
+
+        float highestFitness = 0;
+        Bird bestBird = null;
+        for (Bird bird : savedBirds) {
+            if (bird.getFitness() > highestFitness) {
+                highestFitness = bird.getFitness();
+                bestBird = bird;
+            }
+        }
+
+        for (int i = 0; i < in_training; i++) {
+            getBirds().add(bestBird.copy());
+        }
+        savedBirds.clear();
+    }
+
+    private void normalizeFitness(ArrayList<Bird> birds) {
+        int sum = 0;
+        for (Bird bird : birds) {
+            bird.setScore((float) (Math.pow(bird.getScore(), 2)));
+        }
+
+        for (Bird bird : birds) {
+            sum += bird.getScore();
+        }
+
+        for (Bird bird : birds) {
+            bird.setFitness(bird.getScore() / sum);
+        }
+    }
+
 }
